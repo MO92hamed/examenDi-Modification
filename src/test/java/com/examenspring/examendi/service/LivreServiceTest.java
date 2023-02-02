bad2dee7d4549e26fcf391a05560cf56a63d6e78
MO92hamed <mouhamedamineelabed@gmail.com>
2023-02-02 16:16:26 +0100

    updated

--- src/test/java/com/examenspring/examendi/service/LivreServiceTest.java
+++ src/test/java/com/examenspring/examendi/service/LivreServiceTest.java
@@ -1,10 +1,16 @@
 package com.examenspring.examendi.service;
 
+import com.examenspring.examendi.model.Livre;
 import com.examenspring.examendi.repository.LivreRepository;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
@@ -17,7 +23,13 @@ public class LivreServiceTest {
     LivreRepository livreRepository;
 
     @Test
-    public void whenFindAllLivres_ReturnLivreList(){
+    public void findByTitleNotFoundTest(){
+        Optional<Livre> livreParam = Optional.of(new Livre(18L,"Welcome to task 4", "lfkjdfghdfgldfgjkdfgh"));
+        Mockito.when(livreRepository.findByTitle(Mockito.anyString())).thenReturn(livreParam);
 
+        Optional<Livre> livre = livreService.findLivreByTitle("Welcome to task 4");
+
+        assertEquals(true, livre.isPresent());
+        assertEquals("Welcome to task 4", livre.get().getTitle());
     }
 }
